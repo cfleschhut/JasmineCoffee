d6a79f2167dec30f1a43ab300dc27ddf4fbd6b2b
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-03-04 23:30:47 +0100

    initial rails 4.0.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasmineCoffee::Application.config.secret_key_base = '5bb13d7f81a39c82b26bac6a9b82ae180032f541dbdd2803c1e55c994ba7eebed3e7756aa802452280f46cac858915f0fc101ca8ab8c5be6566b1688ed54ab30'
